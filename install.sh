949e0b45ace7eda4ec00b8a8493801e1c9c2b520
MrZlo <zlo.alex.it@gmail.com>
2021-04-02 19:08:42 +0300

    Repair install

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ cd ntui
 for f in *.sh
 do
 	mv -- "$f" "${f%.sh}"
-	chmod +x "%f"
-	sudo mv "%f" /usr/local/bin
+done
+
+for f in *
+do
+	chmod +x "$f"
+	sudo mv "$f" /usr/local/bin
 	rm "$f"
 done
 
