38d11469ee8c5d4138c202b966d6315e484be652
MrZlo <zlo.alex.it@gmail.com>
2021-04-03 20:03:55 +0300

    FUCKING SHELL SCRIPTING

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+# defining your shell
+if [[ $SHELL = "/bin/zsh" ]]
+then
+	my_shell="zsh"
+elif [[ $SHELL = "/bin/bash" ]]
+then
+	my_shell="bash"
+fi
+
+echo "1"
+
+# making own directory
+sudo mkdir /usr/local/bin/lsc
+# adding it into $PATH
+sed -i -e '$a# adding a path for LSC\nexport PATH="$PATH:/usr/local/bin/lsc"' $HOME/.$my_shell"rc"
+source $HOME/.$my_shell"rc"
+
+echo "2"
+
 cd ntui
+
+echo "3"
 
 # changing extention
 for f in *.sh
@@ -8,12 +29,7 @@ do
 	mv -- "$f" "${f%.sh}"
 done
 
-# making own directory
-mkdir /usr/local/bin/lsc
-# adding it into $PATH
-# $HOME/.$0rc
-sed -i -e -f sed_commands #HOME/.$0rc	
-
+echo "4"
 
 # installing in PATH directory and making they executable
 for f in *
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# defining your shell
+if [[ $SHELL = "/bin/zsh" ]]
+then
+	my_shell="zsh"
+elif [[ $SHELL = "/bin/bash" ]]
+then
+	my_shell="bash"
+fi
+
 cd ..
 rm -rf lsc
 
 cd /usr/local/bin
 sudo rm -rf lsc
+
+sed -i -e 's!# adding a path for LSC\nexport PATH="$PATH:/usr/local/bin/lsc"!!' $HOME/.$my_shell"rc"
+source $HOME/.$my_shell"rc"
 
 echo "DONE!"
