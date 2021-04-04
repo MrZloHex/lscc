a0ebce29fa0989a036378d31dfbf7ff47928f846
MrZlo <zlo.alex.it@gmail.com>
2021-04-04 19:55:24 +0300

    ITS WORK

--- install.sh
+++ install.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-# defining your shell
-if [[ $SHELL = "/bin/zsh" ]]
-then
-	my_shell="zsh"
-elif [[ $SHELL = "/bin/bash" ]]
-then
-	my_shell="bash"
-fi
-
-echo "1"
-
 # making own directory
-sudo mkdir /usr/local/bin/lsc
+#sudo mkdir /usr/local/bin/lsc
 # adding it into $PATH
-sed -i -e '$a# adding a path for LSC\nexport PATH="$PATH:/usr/local/bin/lsc"' $HOME/.$my_shell"rc"
-source $HOME/.$my_shell"rc"
-
-echo "2"
+#sed -i -e '$a# adding a path for LSC\nexport PATH="$PATH:/usr/local/bin/lsc"' $HOME/.$my_shell"rc"
+#source $HOME/.$my_shell"rc"
 
 cd ntui
-
-echo "3"
 
 # changing extention
 for f in *.sh
@@ -29,13 +14,11 @@ do
 	mv -- "$f" "${f%.sh}"
 done
 
-echo "4"
-
 # installing in PATH directory and making they executable
 for f in *
 do
 	chmod +x "$f"
-	sudo mv "$f" /usr/local/bin/lsc
+	sudo mv "$f" /usr/local/bin
 done
 
 cd ..
