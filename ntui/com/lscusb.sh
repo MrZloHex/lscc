0ef11bdf140e506d200ae39c002617c850bb59fe
MrZlo <zlo.alex.it@gmail.com>
2021-04-05 23:03:40 +0300

    testing flags

--- ntui/com/lscusb.sh
+++ ntui/com/lscusb.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# taking flags and options
+
+while getopts vs:d:DtV: flag
+do
+	case "${flag}" in
+		v) opt=${OPTARG}
+			;;
+		*) echo "FLAGS"
+			;;
+	esac
+done
+
+
+
 while [[ $key != "q" ]]
 do
 	lsusb
