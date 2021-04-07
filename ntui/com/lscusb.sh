89068094a35551c1580895ead62b6986a24c363a
MrZlo <zlo.alex.it@gmail.com>
2021-04-07 19:52:20 +0300

    testing kernel fun

--- ntui/com/lscusb.sh
+++ ntui/com/lscusb.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+vERBOSE=unset
+sHOW=unset
+Id=unset
+DEVICE=unset
+tREE=unset
+VERSION=unset
+
+
 # calling function
 call_function()
 {
@@ -23,25 +31,19 @@ call_function()
 
 
 # taking flags and options
-while getopts vs:d:DtV: flag
+PARSED_ARGUMENTS=$(getopt -a -n lscusb -o vs:d:D:tV --long verbose, tree, version -- "$@")
+eval set -- "$PARSED_ARGUMENTS"
+
+while :
 do
-	case "${flag}" in
-		v) call_function v
-			;;
-		s) call_function s ${OPTARG}
-			;;
-		d) call_function d ${OPTARG}
-			;;
-		D) call_function D
-			;;
-		t) call_function t
-			;;
-		V) call_function V ${OPTARG}
-			;;
-		\?) call_function
-			;;
+	case "$1" in
+		-v | --verbose) vERBOSE="-v"	; shift   ;;
+		-s)		sHOW="-s $2"	; shift 2 ;;
+		-d) 		Id="-d $2" 	; shift 2 ;;
+		-D) 		DEVICE="-D $2"	; shift 2 ;;
+		-t | --tree )	tREE="-t"	; shift   ;;
+		-V | --version)	VERSION="-V"	; shift   ;;
+		--)	shift; break ;;
+		*) echo "INVALID FLAG" ;;
 	esac
-done
-
-
-
+done	
