000ba7ca1957e28c93ac36a3b5916945002a518a
MrZlo <zlo.alex.it@gmail.com>
2021-04-08 11:22:24 +0300

    lsattr test opt

--- ntui/com/lscattr.sh
+++ ntui/com/lscattr.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-while [[ $key != "q" ]]
+# all flags
+RECURSION=""
+VERSION=""
+aLL=""
+dIRECTORIES=""
+lONG=""
+pROJECT=""
+vGENERATION=""
+
+
+# calling function
+call_function()
+{
+	while [[ $key != "q" ]]
+	do
+		lsattr $RECURSION $VERSION $aLL $dIRECTORIES $lONG $pROJECT $vGENERATION
+		read -s -n 1 key
+		clear
+	done
+}
+
+
+# taking all options and flags
+eval set -- `getopt -o RVadlpv -- "$@"`
+while :
 do
-	lsattr
-	read -s -n 1 key
-	clear
+	case "$1" in
+		-R) RECURSION="-R"	; shift   ;;
+		-V) VERSION="-V"	; shift   ;;
+		-a) aLL="-a"		; shift   ;;
+		-d) dIRECTORIES="-d"	; shift   ;;
+		-l) lONG="-l"		; shift   ;;
+		-p) pROJECT="-p"	; shift   ;;
+		-v) vGENERATION="-v"	; shift   ;;
+		--)	shift ; break ;;
+		* ) echo "smth goes wrong"
+	esac
 done
+
+call_function
