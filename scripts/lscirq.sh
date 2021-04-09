2fc674a613d9091ed8f8d3db77ab1f0466cc9dad
MrZlo <zlo.alex.it@gmail.com>
2021-04-09 21:48:55 +0300

    lscirq

--- scripts/lscirq.sh
+++ scripts/lscirq.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-while [[ $key != "q" ]]
+noheadings=""
+output=""
+sorts=""
+json=""
+pairs=""
+version=""
+helps=""
+
+call_function()
+{
+	while [[ $key != "q" ]]
+	do
+		lsirq $noheadings $output $sorts $json $pairs $version $helps
+		read -s -n 1 key
+		clear
+	done
+}
+
+
+eval set -- `getopt -o no:s:JPVh --long noheadings,output:,sort:,json,pairs,version,help: -- "$@"`
+while :
 do
-	lsirq
-	read -s -n 1 key
-	clear
+	case "$1" in
+		-n | --noheadings) noheadings="-n"	; shift   ;;
+		-o | --output)	   output="-o $2"	; shift 2 ;;
+		-s | --sort)	   sorts="-s $2"	; shift 2 ;;
+		-J | --json)	   json="-J"		; shift   ;;
+		-P | --pairs)	   pairs="-P" 		; shift   ;;
+		-V | --version)    version="-V"		; shift   ;;
+		-h | --help) 	   helps="-h" 		; shift   ;;
+		--)	shift; break ;;
+		*) echo "INVALID FLAG"
+	esac
 done
