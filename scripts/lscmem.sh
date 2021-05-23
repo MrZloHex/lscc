0346b5ece59e09e88aadda636746e90989355653
MrZloHex <zlo.alex.it@gmail.com>
2021-05-23 21:58:47 +0300

    lsmem

--- scripts/lscmem.sh
+++ scripts/lscmem.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
 
-while [[ $key != "q" ]]
+aLL=""
+bYTES=""
+hELP=""
+JSON=""
+nOHEADINGS=""
+oUTPUT=""
+Ouput_list=""
+PAIRS=""
+rAW=""
+SPLIT=""
+sYSROOT=""
+VERSION=""
+Usummary=""
+
+# calling function
+call_function(){
+	while [[ $key != "q" ]]
+	do
+		lsmem $aLL $bYTES $hELP $JSON $nOHEADINGS $oUTPUT $Ouput_list $PAIRS $rAW $SPLIT $sYSROOT $VERSION $Usummary
+		read -s -n 1 key
+		clear
+	done
+}
+
+# taking flags and options
+eval set -- `getopt -o abhJno:OPrs:S:VU: -- "$@"`
+while :
 do
-	lsmem
-	read -s -n 1 key
-	clear
+	case "$1" in
+		-a | --all)			aLL="-a"					; shift   ;;
+		-b | --bytes)		bYTES="-b"					; shift   ;;
+		-h | --help)		hELP="-h"					; shift   ;;
+		-J | --json)		JSON="-J"					; shift   ;;
+		-n | --noheadings)	nOHEADINGS="-n"				; shift   ;;
+		-o | --output)		oUTPUT="-o $2"				; shift 2 ;;
+		-O | --output-list)	Ouput_list="--output-list"	; shift   ;;
+		-P | --pairs)		PAIRS="-P"					; shift   ;;
+		-r | --raw)			rAW="-r"					; shift   ;;
+		-S | --split)		SPLIT="-S $2"				; shift 2 ;;
+		-s | --sysroot)		sYSROOT="-s $2"				; shift 2 ;;
+		-V | --version)		VERSION="-V"				; shift   ;;
+		-U | --summary)		Usummary="--summary $2"		; shift 2 ;;
+		--)	shift; break ;;
+		*) echo "INVALID FLAG" ;;
+	esac
 done
+
+call_function
